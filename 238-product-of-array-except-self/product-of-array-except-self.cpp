ead44183b0a30e33730d965001af4d37ab3fc59d
Chhayaaprakashh <chhaayaa06@gmail.com>
2026-09-23 00:25:54 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (59.09%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans(n, 1);
-        
-        // First pass: Calculate left/prefix products
-        for (int i = 1; i < n; i++) {
-            ans[i] = ans[i - 1] * nums[i - 1];
+        vector<int> answer(nums.size(),1);
+        int left= 1;
+        int right=1;
+        for (int i=0;i<nums.size();i++){
+           answer[i]= left;
+           left= left*nums[i];
         }
-        
-        // Second pass: Calculate right/suffix products and multiply
-        int right = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            ans[i] *= right;
-            right *= nums[i];
+        for(int j=nums.size()-1;j>=0;j--){
+            
+            answer[j]= answer[j] * right;
+            right= right* nums[j];
         }
-        
-        return ans;
+        return answer;
+
     }
 };
